f23aaa750d622eeb0b3714aaf37d0ff183f49ae8
AhmedHussKhalifa <eng.ahmed_hussein@hotmail.com>
2022-09-02 21:45:03 -0400

    BUG in Cb

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 for model in vgg11 #resnet18 alexNet mobilenet
 do
-	for attack in PGD CW
+	for attack in CW PGD
 	do
 			python3 run.py --Model ${model} --Attack_Method ${attack} --thr 10000 --Batch_size 100
 
